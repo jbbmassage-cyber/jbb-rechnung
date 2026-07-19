bbd1231ae32b92e61ed9eb87538fc17301a5c279
jbbmassage-cyber <jbb_massage@outlook.com>
2026-07-19 16:05:40 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jbb-rechnung-v4-step8-3-tax-above-thanks-20260719-1';
+const CACHE_NAME = 'jbb-rechnung-v4-step8-4-summary-at-table-20260719-1';
 const CORE_ASSETS = [
   "./",
   "./index.html"
