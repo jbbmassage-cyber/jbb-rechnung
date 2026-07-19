e714ec6bebdd46154663cc6ee98d51b812dcf4ff
jbbmassage-cyber <jbb_massage@outlook.com>
2026-07-19 15:02:07 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jbb-rechnung-v4-step5-2-transparent-logo-20260719-1';
+const CACHE_NAME = 'jbb-rechnung-v4-step7-8-logo-transparent-20260719-1';
 const CORE_ASSETS = [
   "./",
   "./index.html"
